d462ba073a628f1c3f50ecac9a1a4e46984872f3
Daniel Oliveros <130321634+d4nc0de@users.noreply.github.com>
2024-09-05 01:21:45 -0500

    VIsta implementada

---
+++ src/vista/Vista.py
@@ -0,0 +1,20 @@
+import customtkinter as ctk
+
+class Vista:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Aplicación MVC")
+        
+        # Configura la ventana principal
+        self.root.geometry("300x200")
+        
+        # Etiqueta
+        self.label = ctk.CTkLabel(master=self.root, text="Hola", font=("Arial", 16))
+        self.label.pack(pady=20)
+        
+        # Botón
+        self.boton = ctk.CTkButton(master=self.root, text="Actualizar", command=self.actualizar)
+        self.boton.pack(pady=20)
+
+    def actualizar(self):
+        self.label.config(text="Actualizado")
